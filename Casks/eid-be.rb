5bf275f4a4f3947e1c9168718dd8919a895b8f54
Michael Ford <fanquake@users.noreply.github.com>
2016-07-17 20:16:24 +0800

    [squashme] Update url & pkg

--- Casks/eid-be.rb
+++ Casks/eid-be.rb
@@ -2,13 +2,13 @@ cask 'eid-be' do
   version '4.1.11'
   sha256 '9f70fe7c4be0676af22d7b4bdaa024e731d9b0485cf022bddffe1271ec0f7500'
 
-  url "https://downloads.services.belgium.be/eid/beidbuild-#{version.delete('.')}.dmg"
+  url "https://downloads.services.belgium.be/eid/eID-Quickinstaller-#{version}.dmg"
   name 'Electronic identity card software of Belgium'
   name 'eID Belgium'
   homepage 'http://eid.belgium.be/'
   license :oss
 
-  pkg 'beidbuild-signed.pkg'
+  pkg 'eID-Quickinstaller-signed.pkg'
 
   uninstall pkgutil: 'be.eid.middleware'
 
