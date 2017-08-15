c316997606685aa90cb42e34bc0adcca0f6675cf
Vítor Galvão <info@vitorgalvao.com>
2017-08-15 12:57:57 +0100

    Update and rename eid-it.rb to eid-it-tuscany.rb

--- Casks/eid-it-tuscany.rb
+++ Casks/eid-it-tuscany.rb
@@ -1,12 +1,10 @@
-cask 'eid-it' do
+cask 'eid-it-tuscany' do
   version '6.37.05'
   sha256 'a4ce3256f361a4b40b6958be599c69fedb18a42f695f207e86b64810a49746e4'
 
   url 'http://www.regione.toscana.it/documents/10180/13787134/Software+carta+1+MAC/a58c2031-449c-48cc-8c31-24c5dc2443bc'
-  name 'IDProtect Manager'
-  name 'IDProtect PINTool'
-  name 'Electronic identity card software of Italy'
-  name 'eID Italy'
+  name 'Electronic identity card software of Italy: Tuscany'
+  name 'eID Italy: Tuscany'
   homepage 'http://www.regione.toscana.it/servizi-online/servizi-sicuri/carta-sanitaria-elettronica'
 
   installer script: {
