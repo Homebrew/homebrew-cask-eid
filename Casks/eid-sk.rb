e2bf0a299db5885eb302b355170bde1c2f1343c9
commitay <commitay@users.noreply.github.com>
2017-10-31 13:05:11 +1000

    eid-sk

--- Casks/eid-sk.rb
+++ Casks/eid-sk.rb
@@ -1,11 +1,12 @@
 cask 'eid-sk' do
-  version '1.9.4'
-  sha256 '89c3420c7e4f82f8203d2caa91934cddb68f065e0d83f3073db0fdb436f98722'
+  version :latest
+  sha256 :no_check
 
-  # eidas.minv.sk was verified as official when first introduced to the cask
+  # eidas.minv.sk/TCTokenService/download was verified as official when first introduced to the cask
   url 'https://eidas.minv.sk/TCTokenService/download/mac/eIDklient.dmg'
-  name 'eID Slovakia'
   name 'eID Klient'
+  name 'Electronic identity card software for Slovakia'
+  name 'eID Slovakia'
   homepage 'https://www.slovensko.sk/sk/titulna-stranka'
 
   app 'eIDklient.app'
