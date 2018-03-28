d7d98bfcf3ba5c9fa6f6c1940d4d42ee95f25a52
commitay <commitay@users.noreply.github.com>
2018-03-28 22:54:22 +1000

    Update eid-ch.rb

--- Casks/eid-ch.rb
+++ Casks/eid-ch.rb
@@ -1,11 +1,11 @@
 cask 'eid-ch' do
-  version :latest
-  sha256 :no_check
+  version '3.7'
+  sha256 'ca075ec64e03a958968f3421c41e71aced205af11a94a147193badf2148643f7'
 
-  url 'https://www.postsuisseid.ch/macinstaller'
-  name 'Electronic identity card software for Switzerland (SuisseID)'
-  name 'eID Switzerland'
+  url "https://www.postsuisseid.ch/images/setup/suisseid-mac-#{version}.dmg"
   name 'SuisseID'
+  name 'Electronic identity card software for Switzerland'
+  name 'eID Switzerland'
   homepage 'https://www.postsuisseid.ch/'
 
   pkg 'Install_SuisseID.pkg'
