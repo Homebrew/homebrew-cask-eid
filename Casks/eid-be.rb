4128d9ec85e7b9bf464900853d893caf011b02bc
commitay <commitay@users.noreply.github.com>
2018-04-28 22:14:32 +1000

    Update eid-be.rb

--- Casks/eid-be.rb
+++ Casks/eid-be.rb
@@ -5,11 +5,11 @@ cask 'eid-be' do
   url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}.dmg"
   name 'Electronic identity card software of Belgium'
   name 'eID Belgium Quickinstaller'
-  homepage 'http://eid.belgium.be/'
+  homepage 'https://eid.belgium.be/'
 
   pkg 'eID-Quickinstaller-signed.pkg'
 
-  uninstall pkgutil: 'be.eid.middleware'
+  uninstall pkgutil: 'be.eid.*'
 
   caveats do
     depends_on_java
